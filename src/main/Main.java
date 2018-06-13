b084c404d551f7fc98194bcbcbb7ca530d7ed6fb
tower-hufham <towerhufham@yahoo.com>
2018-06-13 19:01:34 -0400

    css babey

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,19 +1,11 @@
 package main;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -21,25 +13,31 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        System.setProperty("prism.lcdtext", "false");
         primaryStage.setTitle("JavaFX Welcome");
 
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
+
         grid.setPadding(new Insets(25, 25, 25, 25));
 
         Text title = new Text("Welcome");
         title.setId("title");
-        grid.add(title, 0, 0, 2, 1);
-        Label userName = new Label("User Name:");
-        grid.add(userName, 0, 1);
-        TextField userTextField = new TextField();
-        grid.add(userTextField, 1,1);
-        Label pw = new Label("Password:");
-        grid.add(pw, 0, 2);
-        PasswordField pwBox = new PasswordField();
-        grid.add(pwBox, 1, 2);
+        grid.add(title, 0, 0, 16, 1);
+
+        for (int j = 0; j < 12; j++) {
+            for (int i = 0; i < 16; i++) {
+                Label dummyhex = new Label("ce");
+                if (50 < (i + j * 16) && (i + j * 16) < 79) {
+                    dummyhex.setId("highlighted-hex");
+                } else {
+                    dummyhex.setId("default-hex");
+                }
+                grid.add(dummyhex, i, j+1, 1, 1);
+            }
+        }
 
         Scene scene = new Scene(grid, 800, 600);
         primaryStage.setScene(scene);
