45b0643ffa34934993a9339d2d6f2324240bcc15
tower-hufham <towerhufham@yahoo.com>
2018-06-13 17:49:17 -0400

    figuring out javafx
    :stars: :stars: :stars:

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,32 +3,48 @@ package main;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        primaryStage.setTitle("JavaFX Welcome");
 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        Text title = new Text("Welcome");
+        title.setId("title");
+        grid.add(title, 0, 0, 2, 1);
+        Label userName = new Label("User Name:");
+        grid.add(userName, 0, 1);
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1,1);
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
 
-        Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Hello World!");
+        Scene scene = new Scene(grid, 800, 600);
         primaryStage.setScene(scene);
+        scene.getStylesheets().add(Main.class.getResource("style.css").toExternalForm());
+
         primaryStage.show();
     }
     public static void main(String[] args) {
